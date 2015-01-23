dd2f62c3037682e90c67734f54a259af0399ae96
unknown <lizhonghua@lizhonghua-D2.corp.qihoo.net>
2015-01-23 20:00:24 +0800

    first commit

---
+++ start.php
@@ -0,0 +1,13 @@
+<?php  
+/**
+ * @file        start.php
+ * @author       zhonghuali241@163.com
+ * @date        2015-1-21
+ * @desc
+ */
+
+require('engine.php');
+
+
+$engine = new Engine();
+$engine->runCheck();
